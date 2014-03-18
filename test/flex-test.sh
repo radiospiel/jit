a1c3def9de88a819515174d879719ee7bd87d5c4
radiospiel <eno@open-lab.org>
2014-03-18 15:36:01 +0000

    Fixes tests

--- test/flex-test.sh
+++ test/flex-test.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/env roundup
-describe "test jit C mode"
+describe "test jit flex mode"
 
 HERE=$(dirname $1)
 PATH=$(cd $HERE/.. && pwd)/bin:$PATH
 
+
 # verify parameter passing
-it_works_with_c() {
+it_works_with_flex() {
   # clean the compiled script, if it exists
-  jit --clear $HERE/c.jit
+  jit --clear $HERE/flex.jit
   
   # rebuild and run the c test script
-  welcome=$($HERE/c.jit)
-  test "$welcome" = "jit says hi"
-
-  # rerun the c test script
-  welcome=$($HERE/c.jit)
-  test "$welcome" = "jit says hi"
+  result=$(echo "foobar" | $HERE/flex.jit)
+  test "$result" = "ooa"
 }
