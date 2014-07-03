dfa0468d174c83cadc57bee6c3e5865ca7ca6607
eno <eno@open-lab.org>
2014-07-03 16:27:17 +0200

    Fix arg0 passing

--- test/base-test.sh
+++ test/base-test.sh
@@ -5,6 +5,12 @@ HERE=$(dirname $1)
 PATH=$(cd $HERE/.. && pwd)/bin:$PATH
 
 # verify parameter passing
+it_gets_arg0() {
+  r=$($HERE/arg0.jit)
+  test "$r" = "$HERE/arg0.jit"
+}
+
+# verify parameter passing
 it_gets_args() {
   r=$($HERE/args.jit)
   test "$r" = "1"
