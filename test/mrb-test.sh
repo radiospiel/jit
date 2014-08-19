d5b540dfe151037f787197e5cf4d3817b5c45a1c
eno <eno@open-lab.org>
2014-08-19 17:11:12 +0200

    mruby: add support for exit code

--- test/mrb-test.sh
+++ test/mrb-test.sh
@@ -6,12 +6,29 @@ PATH=$(cd $HERE/.. && pwd)/bin:$PATH
 
 it_works_with_mrb() {
   # rebuild and run the mrb test script
-  welcome=$($HERE/mrb.jit)
-  test "$welcome" = "Hello jitted world!"
+  prog=$($HERE/mrb.jit)
+  test "$prog" = "Hello jitted world!"
 }
 
 it_gets_arguments() {
   # rebuild and run the mrb test script
-  welcome=$($HERE/mrb.echo.jit foo bar baz)
-  test "$welcome" = "mrb.echo.jit:foo bar baz"
+  prog=$($HERE/mrb.echo.jit foo bar baz)
+  test "$prog" = "mrb.echo.jit:foo bar baz"
+}
+
+it_sets_exitcode() {
+  # rebuild and run the mrb test script
+  if $HERE/mrb.exit.jit 1 ; then
+    false Exit code must be 1
+  else
+    test $? = 1
+  fi
+
+  if $HERE/mrb.exit.jit 2 ; then
+    false Exit code must be 2
+  else
+    test $? = 2
+  fi
+
+  $HERE/mrb.exit.jit 0
 }
