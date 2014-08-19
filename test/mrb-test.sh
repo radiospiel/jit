070398c0b6a313ed368e6a1930d570c932be3766
eno <eno@open-lab.org>
2014-08-19 16:49:39 +0200

    Add mruby support for arguments

--- test/mrb-test.sh
+++ test/mrb-test.sh
@@ -4,11 +4,14 @@ describe "test jit mrb mode"
 HERE=$(dirname $1)
 PATH=$(cd $HERE/.. && pwd)/bin:$PATH
 
-# verify parameter passing
 it_works_with_mrb() {
-  echo $PATH
-
-  # rebuild and run the go test script
+  # rebuild and run the mrb test script
   welcome=$($HERE/mrb.jit)
   test "$welcome" = "Hello jitted world!"
+}
+
+it_gets_arguments() {
+  # rebuild and run the mrb test script
+  welcome=$($HERE/mrb.echo.jit foo bar baz)
+  test "$welcome" = "mrb.echo.jit:foo bar baz"
 }
